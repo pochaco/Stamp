a8b70bb478c3fdd6b8ace7fe2ca736148e59a513
Miu Yamamoto <mi.oct09@gmail.com>
2020-09-14 15:13:37 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by Yamamoto Miu on 2020/09/14.
+//  Copyright © 2020 Yamamoto Miu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
